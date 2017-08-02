a76cb5c70fd1e800ec3a7056241c316f8ccb54d2
Anthony Weston <anthonyelliotweston@gmail.com>
2017-08-02 01:01:20 -0400

    exploring the basics of decision trees

---
+++ decision-tree/basic_decision_tree.py
@@ -0,0 +1,19 @@
+from sklearn.datasets import load_iris
+from sklearn import tree
+
+iris = load_iris()
+d_tree_gini = tree.DecisionTreeClassifier(criterion='gini', min_samples_split = 5)
+d_tree_gini = d_tree_gini.fit(iris.data, iris.target)
+
+from pydotplus import graphviz
+
+dot_data = tree.export_graphviz(d_tree_gini, out_file= 'tree_gini.dot') 
+graph = graphviz.graph_from_dot_file('tree_gini.dot') 
+graph.write_pdf("iris_gini.pdf") 
+
+d_tree_entropy = tree.DecisionTreeClassifier(criterion='entropy', min_samples_split = 5)
+d_tree_entropy = d_tree_entropy.fit(iris.data, iris.target)
+
+dot_data = tree.export_graphviz(d_tree_entropy, out_file= 'tree_entropy.dot') 
+graph = graphviz.graph_from_dot_file('tree_entropy.dot') 
+graph.write_pdf("iris_entropy.pdf") 
